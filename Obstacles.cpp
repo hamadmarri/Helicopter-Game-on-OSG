0cabf23b8be1fb9878993476bd96806b8533610f
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-02 23:32:09 -0600

    obstacles.cpp has been implemented BASIC

---
+++ Obstacles.cpp
@@ -0,0 +1,14 @@
+//
+//  Obstacles.cpp
+//  testOsg
+//
+//  Created by Fares Alotaibi on 2014-03-02.
+//  Copyright (c) 2014 Fares Alotaibi. All rights reserved.
+//
+
+#include "Obstacles.h"
+#include "Model.h"
+
+Obstacles::Obstacles():Model(){
+    
+}

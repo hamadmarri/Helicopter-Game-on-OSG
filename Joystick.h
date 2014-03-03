803fbe4a5d55a388d4dd370fce60aee3b38d1ad7
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-02 20:48:35 -0600

    Second check , Works? -> good , Does not ? -> check the issue

---
+++ Joystick.h
@@ -0,0 +1,16 @@
+//
+//  Joystick.h
+//  testOsg
+//
+//  Created by Fares Alotaibi on 2014-03-02.
+//  Copyright (c) 2014 Fares Alotaibi. All rights reserved.
+//
+
+#ifndef testOsg_Joystick_h
+#define testOsg_Joystick_h
+
+class Joystick {
+    
+};
+
+#endif

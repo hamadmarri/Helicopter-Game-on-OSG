302a2838606e6ed2f4d8f64b85b867a8c7088c6d
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-02 20:46:50 -0600

    Testing the BuitBuckt to commit first header file

---
+++ Helicopter.h
@@ -0,0 +1,17 @@
+//
+//  Helicopter.h
+//  testOsg
+//
+//  Created by Fares Alotaibi on 2014-03-02.
+//  Copyright (c) 2014 Fares Alotaibi. All rights reserved.
+//
+
+#ifndef testOsg_Helicopter_h
+#define testOsg_Helicopter_h
+
+class Helicopter {
+    
+};
+
+
+#endif

35ec13d2b3270aaa0f2aec66f56224f5bc48fdfd
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-02 21:19:24 -0600

    some modification NOT Complete or Tested , just to show the work at this point

--- Obstacles.h
+++ Obstacles.h
@@ -1,17 +1,18 @@
 //
-//  Rotor.h
+//  Obstacles.h
 //  testOsg
 //
 //  Created by Fares Alotaibi on 2014-03-02.
 //  Copyright (c) 2014 Fares Alotaibi. All rights reserved.
 //
 
-#ifndef testOsg_Rotor_h
-#define testOsg_Rotor_h
+#ifndef testOsg_Obstacles_h
+#define testOsg_Obstacles_h
 
-class Rotor{
+class Obstacles{
 public:
-    Rotor();
+    Obstacles();
 };
+
 
 #endif

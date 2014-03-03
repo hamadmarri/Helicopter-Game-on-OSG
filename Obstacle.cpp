5f8e887384b7a62d4f3835d4b72e8c262b3c00cc
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-03 15:37:58 -0600

    obstacles not finished

--- Obstacle.cpp
+++ Obstacle.cpp
@@ -6,13 +6,17 @@
 //  Copyright (c) 2014 Fares Alotaibi. All rights reserved.
 //
 
-#include "Obstacles.h"
+#include "Obstacle.h"
 #include "Model.h"
 
-Obstacles::Obstacles():Model(){
+Obstacle::Obstacle():Model(){
     
 }
 
-osg::Group* Obstacles:: get(){
+osg::Group* Obstacle:: get(){
     return this->PAT;
 }
+void Obstacle::set(osg::Node* a){
+   // return this->addChild(a->get());
+}
+
--- Obstacle.h
+++ Obstacle.h
@@ -6,16 +6,17 @@
 //  Copyright (c) 2014 Fares Alotaibi. All rights reserved.
 //
 
-#ifndef testOsg_Obstacles_h
-#define testOsg_Obstacles_h
+#ifndef testOsg_Obstacle_h
+#define testOsg_Obstacle_h
 
 
 #include "Model.h"
-// constructing the obstacles class
-class Obstacles : public Model{
+// constructing the obstacle class
+class Obstacle : public Model{
 public:
-    Obstacles();
+    Obstacle();
     osg::Group* get();
+    void set(osg::Node* a);
 };
 
 

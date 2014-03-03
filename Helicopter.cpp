622a064754d5ed48e098bc8d67772e6a8f79a9dc
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-02 22:13:32 -0600

    some modification NOT Complete or Tested , just to show the work at this point

---
+++ Helicopter.cpp
@@ -0,0 +1,14 @@
+//
+//  Helicopter.cpp
+//  testOsg
+//
+//  Created by Fares Alotaibi on 2014-03-02.
+//  Copyright (c) 2014 Fares Alotaibi. All rights reserved.
+//
+
+#include "Helicopter.h"
+
+
+Helicopter::Helicopter(){
+    
+}

d35b20bd47c317d6bf54c7341bc113476148e95f
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-09 16:51:17 -0600

    Configuration.cpp other functions have been implemented (initScreanSizeSettings() + initKeySettings + getScreenWidth() + getScreenHeight() + getKeySettings() )
    almost done with this class at this point.

--- Configuration.cpp
+++ Configuration.cpp
@@ -20,6 +20,61 @@ Configuration* Configuration::getInstance(){
 void Configuration::initialize(){
     // get screen size
     initScreanSizeSettings();
-    // get ket settings 
+    // get ket settings
     initKeySettings();
+}
+
+// 
+
+void Configuration::initScreanSizeSettings() {
+	osg::GraphicsContext::WindowingSystemInterface* wsi =
+	osg::GraphicsContext::getWindowingSystemInterface();
+	if (!wsi) {
+		osg::notify(osg::NOTICE)<<"Error, no WindowSystemInterface available, cannot create windows."<<std::endl;
+	} else {
+		
+		wsi->getScreenResolution(osg::GraphicsContext::ScreenIdentifier(0),
+								 Configuration::getInstance()->screenWidth, Configuration::getInstance()->screenHeight);
+	}
+}
+
+
+
+void Configuration::initKeySettings() {
+	std::ifstream inFile;
+    // open the file we provided 
+	inFile.open("settings.txt");
+	
+	inFile.get(Configuration::getInstance()->keySettings.resetJoystick);
+	inFile.ignore(500, '\n');
+    
+	inFile.get(Configuration::getInstance()->keySettings.zeroRotorSpeed);
+	inFile.ignore(500, '\n');
+	
+	inFile.get(Configuration::getInstance()->keySettings.decreaseRotorSpeed);
+	inFile.ignore(500, '\n');
+	
+	inFile.get(Configuration::getInstance()->keySettings.increaseRotorSpeed);
+	inFile.ignore(500, '\n');
+	
+	inFile.get(Configuration::getInstance()->keySettings.neutralRotorMode);
+	
+	inFile.close();
+}
+
+
+
+unsigned int Configuration::getScreenWidth() {
+	return Configuration::getInstance()->screenWidth;
+}
+
+
+
+unsigned int Configuration::getScreenHeight() {
+	return Configuration::getInstance()->screenHeight;
+}
+
+
+KeySettings Configuration::getKeySettings() {
+	return Configuration::getInstance()->keySettings;
 }

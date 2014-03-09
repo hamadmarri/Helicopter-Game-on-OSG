47eac4860ae52c147be017efdf23013d15e98a8a
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-09 17:25:24 -0600

    The Controller.cpp has been modified handle function + addObserver + RemoveObserver + NotifyAll

--- Controller.cpp
+++ Controller.cpp
@@ -4,34 +4,54 @@
 //
 //  Created by Hamad Almarri on 3/3/2014.
 //
+// Edited and Modified by Alotaibi, Fares on 3/9/2014
 
+// I have changed the structure now because we are dealing with event class.
 
 #include "Controller.h"
 
-
 bool Controller::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa, osg::Object*, osg::NodeVisitor*) {
-	switch (ea.getEventType()) {
-		case osgGA::GUIEventAdapter::KEYDOWN:
-			switch(ea.getKey()) {
-				case 'a': case 'A':
-					NotifyAll('a');
-					break;
-				case 'w': case 'W':
-					NotifyAll('w');
-					break;
-				case 'd': case 'D':
-					NotifyAll('d');
-					break;
-				case 's': case 'S':
-					NotifyAll('s');
-					break;
-			}
-			break;
-		case osgGA::GUIEventAdapter::KEYUP:
-			NotifyAll('0');
-			break;
+	
+	auto e = ea.getEventType();
+	auto k = tolower(ea.getKey());
+	
+	if (e == osgGA::GUIEventAdapter::KEYDOWN) {
+        if (k == osgGA::GUIEventAdapter::KEY_Left)
+            NotifyAll(Event(EventType::MOVE_LEFT));
+		
+        else if (k == osgGA::GUIEventAdapter::KEY_Up)
+            NotifyAll(Event(EventType::MOVE_FORWARD));
+        
+        else if (k == osgGA::GUIEventAdapter::KEY_Right)
+            NotifyAll(Event(EventType::MOVE_RIGHT));
+        
+        else if (k == osgGA::GUIEventAdapter::KEY_Down)
+            NotifyAll(Event(EventType::MOVE_BACKWARD));
+        
+        else if (k == Configuration::getInstance()->getKeySettings().resetJoystick)
+            NotifyAll(Event(EventType::CENTER));
+        
+        else if (k == Configuration::getInstance()->getKeySettings().zeroRotorSpeed)
+            NotifyAll(Event(EventType::SWITCH_OFF));
+        
+        else if (k == Configuration::getInstance()->getKeySettings().decreaseRotorSpeed)
+            NotifyAll(Event(EventType::DECREASE));
+        
+        else if (k == Configuration::getInstance()->getKeySettings().increaseRotorSpeed)
+            NotifyAll(Event(EventType::INCREASE));
+        
+        else if (k == Configuration::getInstance()->getKeySettings().neutralRotorMode)
+            NotifyAll(Event(EventType::STAY_NEUTRAL));
 	}
-	return false;
+	else if (e == osgGA::GUIEventAdapter::KEYUP) {
+		NotifyAll(Event(EventType::CENTER));
+	}
+	//Detect a mouse movement
+	else if ( e == osgGA::GUIEventAdapter::MOVE) {
+		NotifyAll(Event(EventType::MOUSE_MOVEMENT, ea.getX(), ea.getY()));
+	}
+    
+    return false;
 }
 
 
@@ -49,14 +69,10 @@ void Controller::RemoveObserver(Observer *observer) {
 }
 
 
-void Controller::NotifyAll(char event) {
+void Controller::NotifyAll(Event event) {
 	for (auto observer = this->observers.begin(); observer != this->observers.end(); observer++)
 		(*observer)->Observe(event);
 }
-
-
-
-
 
 
 

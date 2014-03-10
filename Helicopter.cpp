8c490998b4a65746635017af6c231e3ff7060b9a
Hamad Almarri <hamadmarri@yahoo.com>
2014-03-09 18:48:33 -0600

    helicopter handle the rotor magnitude

--- Helicopter.cpp
+++ Helicopter.cpp
@@ -4,6 +4,7 @@
 //
 //  Created by Fares Alotaibi on 2014-03-02.
 //	Credit: http://scenemodels.flightgear.org/objectview.php?id=3303278
+//			http://hyperphysics.phy-astr.gsu.edu/hbase/airfri.html
 //
 
 #include "Helicopter.h"
@@ -11,26 +12,24 @@
 // constructor of the helicopter from the model class
 
 Helicopter::Helicopter() : Model() {
-    joystick = new Joystick();
+    this->joystick = new Joystick();
+	this->rotor = new Rotor();
    
     this->node->addChild(osgDB::readNodeFile("3124/EC-135_Douane.ac"));
-	this->PAT->setPosition(osg::Vec3f(100.0, 0.0, 100.0));
-	   
+	this->PAT->setPosition(osg::Vec3f(-10.0, -3000.0, -2000.0));
 }
 
-void Helicopter:: Observe(char event){
+void Helicopter:: Observe(Event event){
     static float y = 0;
 	static double t = 0;
+	float viscousResistance = -6 * WORLD_PI * 4;
+
 	
-	
-    
-	this->motion.netForce.set(this->joystick->getForce().operator*(9.8)
-							  + osg::Vec3f(0.0, 0.0, -1).operator*(9.8)
-							  - osg::Vec3f(0.99 * this->motion.lastVelocity.x(),
-										   0.99 * this->motion.lastVelocity.y(),
-										   2.99 * this->motion.lastVelocity.z())
+	this->motion.netForce.set(this->joystick->getForce().operator*(this->rotor->magnitude)
+							  + osg::Vec3f(0.0, 0.0, -1).operator*(WORLD_GRAVITY)
+							  + this->motion.lastVelocity.operator*(viscousResistance)
 							  );
-	
+
 	
 	t += 0.0166;
 	osg::Vec3f nextPosition = this->motion.calculate_position_at(t);
@@ -41,7 +40,6 @@ void Helicopter:: Observe(char event){
     this->PAT->setPosition(osg::Vec3f(this->PAT->getPosition().x() + nextPosition.y() * cosf(y),
 										   this->PAT->getPosition().y() + nextPosition.z(),
 										   this->PAT->getPosition().z() + nextPosition.x() * cosf(y)));
-    
     
 }
 

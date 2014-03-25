4a4cbee6718e87e9cd937fd104483064a7ba02b7
Hamad Almarri <hamadmarri@yahoo.com>
2014-03-25 16:03:16 -0600

    gathering all rotor commands in one header file

---
+++ RotorCommands.h
@@ -0,0 +1,12 @@
+
+
+#ifndef HelicopterProject_RotorCommands_h
+#define HelicopterProject_RotorCommands_h
+
+#include "RotorDecrease.h"
+#include "RotorIncrease.h"
+#include "RotorNeutral.h"
+#include "RotorSwitchOff.h"
+#include "RotorPercentage.h"
+
+#endif

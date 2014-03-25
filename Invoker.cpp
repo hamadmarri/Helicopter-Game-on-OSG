0663d8bbf47e624f7af84851bec4e51f5d5a8eb7
Hamad Almarri <hamadmarri@yahoo.com>
2014-03-25 14:53:35 -0600

    invoker class is added

---
+++ Invoker.cpp
@@ -0,0 +1,67 @@
+//
+//  Invoker.cpp
+//  HelicopterProject
+//
+//  Created by Hamad Almarri on 2014-03-18.
+//  Copyright (c) 2014 Hamad Almarri. All rights reserved.
+//
+
+#include "Invoker.h"
+
+
+
+Invoker::Invoker() {
+	
+}
+
+
+
+Invoker::~Invoker() {
+	while (!this->commands.empty()) {
+		delete this->commands.front();
+		this->commands.pop();
+	}
+}
+
+
+
+void Invoker::addCommand(Command *cmd) {
+	this->commands.push(cmd);
+}
+
+
+
+bool Invoker::addAndExecuteCommand(Command *cmd) {
+	addCommand(cmd);
+	return execute();
+}
+
+
+
+bool Invoker::execute() {
+	Command *cmd = this->commands.front();
+	
+	if (!cmd->execute())
+		return false;
+	
+	delete cmd;
+	
+	this->commands.pop();
+	return true;
+}
+
+
+
+bool Invoker::executeAll() {
+	while (!this->commands.empty()) {
+		if(!execute())
+			return false;
+	}
+	
+	return true;
+}
+
+
+
+
+
---
+++ Invoker.h
@@ -0,0 +1,70 @@
+//
+//  Invoker.h
+//  HelicopterProject
+//
+//  Created by Hamad Almarri on 2014-03-18.
+//  Copyright (c) 2014 Hamad Almarri. All rights reserved.
+//
+
+#ifndef __HelicopterProject__Invoker__
+#define __HelicopterProject__Invoker__
+
+#include <queue>
+#include "Command.h"
+
+class Invoker {
+public:
+	
+	/*
+	 constructor
+	 */
+	Invoker();
+	
+	
+	
+	/*
+	 deconstructor
+	 */
+	~Invoker();
+	
+	
+	/*
+	 adding a command to the queue
+	 */
+	void addCommand(Command *cmd);
+	
+	
+	/*
+	 adding a command to the queue and execute it
+	 */
+	bool addAndExecuteCommand(Command *cmd);
+	
+	
+	/*
+	 execute one command only that at top of the queue 
+	 and remove it from queue
+	 
+	 return:	if executing a command is failed,
+				then returns false
+	 */
+	bool execute();
+	
+	
+	
+	/*
+	 execute all commands in queue
+	 
+	 return:	if executing a command is failed,
+	 then returns false
+	 */
+	bool executeAll();
+	
+	
+	
+private:
+	std::queue <Command*> commands;
+	
+};
+
+
+#endif /* defined(__HelicopterProject__Invoker__) */

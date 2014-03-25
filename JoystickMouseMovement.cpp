389d8eb065817daa53146ac3c15d0f7119ec4114
Hamad Almarri <hamadmarri@yahoo.com>
2014-03-25 15:29:41 -0600

    JoystickMouseMovement command added

---
+++ JoystickMouseMovement.cpp
@@ -0,0 +1,21 @@
+
+
+#include "JoystickMouseMovement.h"
+
+
+JoystickMouseMovement::JoystickMouseMovement(Joystick *joystick, float xCartesian, float yCartesian) {
+	this->joystick = joystick;
+	this->xCartesian = (Configuration::getScreenWidth() / 2) - xCartesian;
+	this->yCartesian = (Configuration::getScreenHeight() / 2) - yCartesian;
+}
+
+
+
+bool JoystickMouseMovement::execute() {
+	float magnitude = int(sqrt((float) (pow(this->xCartesian, 2.0) + pow(this->yCartesian, 2.0))) / 100) % 15;
+	
+	this->joystick->set_theta(magnitude);
+	this->joystick->set_phi(atan2(this->xCartesian, -1 * this->yCartesian) * 180 / WORLD_PI);
+
+	return true;
+}
---
+++ JoystickMouseMovement.h
@@ -0,0 +1,22 @@
+
+#ifndef __HelicopterProject__JoystickMouseMovement__
+#define __HelicopterProject__JoystickMouseMovement__
+
+#include "Command.h"
+#include "Joystick.h"
+#include "Configuration.h"
+
+
+class JoystickMouseMovement : public Command {
+public:
+	JoystickMouseMovement(Joystick *joystick, float xCartesian, float yCartesian);
+	virtual bool execute();
+	
+private:
+	Joystick *joystick;
+	float xCartesian;
+	float yCartesian;
+};
+
+
+#endif /* defined(__HelicopterProject__JoystickMouseMovement__) */

4b743bfd4ad45478d2c80e9a8ae9122e2f3cbc60
Hamad Almarri <hamadmarri@yahoo.com>
2014-03-25 15:43:09 -0600

    all joystick command files in one header file

---
+++ JoystickCommands.h
@@ -0,0 +1,14 @@
+
+
+#ifndef HelicopterProject_JoystickCommands_h
+#define HelicopterProject_JoystickCommands_h
+
+#include "JoystickMoveLeft.h"
+#include "JoystickMoveForward.h"
+#include "JoystickMoveRight.h"
+#include "JoystickMoveBackward.h"
+#include "JoystickCenter.h"
+#include "JoystickMouseMovement.h"
+#include "JoyStickSetManually.h"
+
+#endif

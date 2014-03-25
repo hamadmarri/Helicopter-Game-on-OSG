268c8346fbd005b9c759620f9c4859ba173c5a17
Hamad Almarri <hamadmarri@yahoo.com>
2014-03-25 14:31:27 -0600

    use chrono in time handler to calculate time between frames

--- TimeHandler.cpp
+++ TimeHandler.cpp
@@ -10,10 +10,28 @@
 #include "TimeHandler.h"
 
 
+TimeHandler::TimeHandler() {
+	this->previousTime = std::chrono::system_clock::now();
+}
+
+
+
 // we are handling eventyType 
 bool TimeHandler::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa, osg::Object*, osg::NodeVisitor*) {
-	if (ea.getEventType() == osgGA::GUIEventAdapter::FRAME)
-        NotifyAll(Event(EventType::UPDATE_POSITION));
+	if (ea.getEventType() == osgGA::GUIEventAdapter::FRAME) {
+		
+		// get current time
+		this->nowTime = std::chrono::system_clock::now();
+		
+		// calculate duration
+		std::chrono::duration<double> elapsedTime = this->nowTime - this->previousTime;
+		
+		// notify all observers
+        NotifyAll(Event(EventType::UPDATE_POSITION, elapsedTime.count()));
+		
+		// shift time
+		this->previousTime = this->nowTime;
+	}
 	
 	return false;
 }
--- TimeHandler.h
+++ TimeHandler.h
@@ -9,15 +9,21 @@
 #ifndef __HelicopterProject__TimeHandler__
 #define __HelicopterProject__TimeHandler__
 
+
+#include <chrono>
 #include <osgGA/GUIEventHandler>
 #include "Observable.h"
 
 
 class TimeHandler : public osgGA::GUIEventHandler, public Observable {
 public:
-	TimeHandler() {}
+	TimeHandler();
 	
 	virtual bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa, osg::Object*, osg::NodeVisitor*);
+	
+private:
+	std::chrono::time_point<std::chrono::system_clock> previousTime;
+	std::chrono::time_point<std::chrono::system_clock> nowTime;
 };
 
 

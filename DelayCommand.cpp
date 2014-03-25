a06849d819f393029dda581ec008cc97271727df
Hamad Almarri <hamadmarri@yahoo.com>
2014-03-25 16:10:35 -0600

    adding delay command

---
+++ DelayCommand.cpp
@@ -0,0 +1,14 @@
+
+
+#include "DelayCommand.h"
+
+
+DelayCommand::DelayCommand(float seconds) {
+	this->seconds = seconds;
+}
+
+
+bool DelayCommand::execute() {
+	std::this_thread::sleep_for(std::chrono::milliseconds(int(1000 * this->seconds)));
+	return true;
+}
---
+++ DelayCommand.h
@@ -0,0 +1,20 @@
+
+
+#ifndef __HelicopterProject__DelayCommand__
+#define __HelicopterProject__DelayCommand__
+
+#include <thread>         // std::this_thread::sleep_for
+#include <chrono>         // std::chrono::seconds
+#include "Command.h"
+
+
+class DelayCommand : public Command {
+public:
+	DelayCommand(float seconds);
+	bool execute();
+	
+private:
+	float seconds;
+};
+
+#endif /* defined(__HelicopterProject__DelayCommand__) */

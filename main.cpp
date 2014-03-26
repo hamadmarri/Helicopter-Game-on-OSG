827ed49694d4f8682148a87c36189b0108ad59cc
Hamad Almarri <hamadmarri@yahoo.com>
2014-03-26 11:35:39 -0600

    adding parseArgs function

--- main.cpp
+++ main.cpp
@@ -4,14 +4,34 @@
 //  Created by Hamad Almarri on 3/2/2014.
 //
 
+
+#include <string>
 #include "Game.h"
+#include "tests/RealTimeTest.h"
+#include "tests/SimulatorTest.h"
 
 
+std::string parseArgs(int argc, char** argv, std::string command) {
+	std::string value;
+	osg::ArgumentParser args(&argc, argv);
+	args.read(command, value );
+	return value;
+}
 
-int main() {
 
-	Game test;
-    test.initialize();
+int main(int argc, char** argv) {
+	
+//	SimulatorTest t;
+//	
+//	t.run();
+	
+	Game game;
+	
+	game.initialize();
+//	game.run(parseArgs(argc, argv, "--script").c_str());
+	game.run();
+	
+
 	
 	return 0;
 }

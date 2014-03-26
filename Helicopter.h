bcfa5a54cd18f7af1e8c3e0d4e23970600e16eb3
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-26 15:46:46 -0600

    modification on helicopter class , adding some functions and deconstructor

--- Helicopter.h
+++ Helicopter.h
@@ -11,18 +11,29 @@
 #ifndef __HelicopterProject__Helicopter_h__
 #define __HelicopterProject__Helicopter_h__
 
+#include <osgViewer/Viewer>
+#include "Configuration.h"
 #include "Joystick.h"
 #include "Rotor.h"
 #include "Model.h"
 #include "Observer.h"
 #include "RK4.h"
+#include "EulerPhysics.h"
 #include "WorldConstants.h"
 
 
 class Helicopter : public Model, public Observer {
 public:
     Helicopter();
+    ~Helicopter();
     void Update(Event event);
+    
+    // override
+	void setPosistion(osg::Vec3f newPos);
+	void reset();
+    
+	osg::Vec3f getVelocity();
+	osg::Vec3f getAcceleration();
 	
 	Joystick* getJoystick();
 	Rotor* getRotor();

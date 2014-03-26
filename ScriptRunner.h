02d1e746d107d81532e5fdee9a63249c68b84d8e
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-25 19:03:48 -0600

    ScriptRunner Class, I will work on today and I will continue working tomorrow

---
+++ ScriptRunner.h
@@ -0,0 +1,21 @@
+//
+//  ScriptRunner.h
+//  testOsg
+//
+//  Created by Fares Alotaibi on 2014-03-25.
+//
+//  ScriptRunner that will read from a file. 
+
+#ifndef testOsg_ScriptRunner_h
+#define testOsg_ScriptRunner_h
+
+class ScriptRunner {
+public:
+    
+    
+private:
+    
+};
+
+
+#endif

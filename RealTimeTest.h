1025fa43c48a22c8fda7ae9d1b55401873c2d554
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-26 20:28:51 -0600

    RealTimeTest class has been initialized , and I  have created an  instance of a Game class to be tested. Member functions will be added soon and the implementation of it.

---
+++ RealTimeTest.h
@@ -0,0 +1,29 @@
+//
+//  RealTimeTest.h
+//  testOsg
+//
+//  Created by Fares Alotaibi on 2014-03-26.
+//
+//
+
+#ifndef testOsg_RealTimeTest_h
+#define testOsg_RealTimeTest_h
+
+#include <iostream>
+#include <thread>
+#include <assert.h>
+#include "../Game.h"
+#include "../AllCommands.h"
+
+class RealTimeTest {
+public:
+    
+    
+private:
+    
+    static Game game;
+    
+};
+
+
+#endif

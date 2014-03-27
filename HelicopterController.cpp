8b95225c3770dbbb4fa64ea07bdf1ba232f28800
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-03-26 19:35:37 -0600

    HelicopterController.cpp , first constructor implementation , no errors

---
+++ HelicopterController.cpp
@@ -0,0 +1,15 @@
+//
+//  HelicopterController.cpp
+//  testOsg
+//
+//  Created by Fares Alotaibi on 2014-03-26.
+//
+//
+
+#include "HelicopterController.h"
+
+
+HelicopterController::HelicopterController(Helicopter *helicopter) {
+	this->helicopter = helicopter;
+}
+

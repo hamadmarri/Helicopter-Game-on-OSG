c81dcfacd42f2452595df9bd7212d5050123d4eb
Hamad Almarri <hamadmarri@yahoo.com>
2014-04-08 20:54:42 -0600

    define SEA_LEVEL, X_REFERENCE, Y_REFERENCE

--- WorldConstants.h
+++ WorldConstants.h
@@ -11,6 +11,8 @@
 #define WORLD_GROUND 0
 #define WORLD_GRAVITY 9.8
 #define WORLD_PI 3.14159265359
-
+#define SEA_LEVEL -99
+#define X_REFERENCE 250
+#define Y_REFERENCE 0
 
 #endif /* defined(__HelicopterProject__WorldConstants__) */

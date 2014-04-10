c708c4e9afa5a4e58dfd0281207e50a428863a10
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-09 21:34:11 -0600

    Adding comments in RotorIncrease.h and RotorIncrease.cpp

--- RotorIncrease.cpp
+++ RotorIncrease.cpp
@@ -1,14 +1,16 @@
 //	See main.cpp file header for credits
+// This is the implementation of RotorIncrease class. The implementation is two function , a constructor that will set the passsing rotor instance to our rotor privat member, and the second function is a boolean function return true if the
+// increaseMagnitude has been called or false if not.
 
 #include "RotorIncrease.h"
 
-
+// a constructor passing a rotor instance and set it to our private member rotor
 RotorIncrease::RotorIncrease(Rotor *rotor) {
 	this->rotor = rotor;
 }
 
 
-
+// return true if the function increaseMagnitude has been called or false if not 
 bool RotorIncrease::execute() {
 	this->rotor->increaseMagnitude();
 	
--- RotorIncrease.h
+++ RotorIncrease.h
@@ -1,4 +1,6 @@
 //	See main.cpp file header for credits
+// RotorIncrease class inheretes from a Command class. The main purpose of this class is to call the increase function
+// of a rotor instance and increase the rotor as a result of that.
 
 #ifndef __HelicopterProject__RotorIncrease__
 #define __HelicopterProject__RotorIncrease__
@@ -6,13 +8,13 @@
 #include "Command.h"
 #include "Rotor.h"
 
-
+// inheretance from Command class public
 class RotorIncrease : public Command {
 public:
-	RotorIncrease(Rotor *rotor);
-	bool execute();
-	
-private:
+	RotorIncrease(Rotor *rotor);            // a constructor passing an instance of a rotor
+	bool execute();                         // a boolean function
+    
+private:                                    // one private member of Rotor instance 
 	Rotor *rotor;
 };
 

eaec7e7f78c554480148452196d0a24ae53ba992
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-09 21:11:26 -0600

    Adding comment to  RotorSetMagnitude.h  and  RotorSetMagnitude.cpp

--- RotorSetMagnitude.h
+++ RotorSetMagnitude.h
@@ -1,3 +1,9 @@
+// RotorSetMagnitude class. The main purpose of this class is to set any value to the rotor magnitude and update
+// the magnitude from that value.
+// RotorSetMagnitude class inherites from Command classs public.
+
+
+
 
 #ifndef __HelicopterProject__RotorSetMagnitude__
 #define __HelicopterProject__RotorSetMagnitude__
@@ -8,10 +14,11 @@
 
 class RotorSetMagnitude : public Command {
 public:
-	RotorSetMagnitude(Rotor *rotor, float magnitude);
-	bool execute();
+    
+	RotorSetMagnitude(Rotor *rotor, float magnitude);  // main constructor passing rotor instance, and a float
+	bool execute();                                    // boolean execute function
 	
-private:
+private:                    // two parameters float, and rotor to set the magnitude 
 	Rotor *rotor;
 	float magnitude;
 };

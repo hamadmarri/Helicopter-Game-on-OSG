9ab43a95a840a713fc91dd77d838dfe80d4aac31
Hamad Almarri <hamadmarri@yahoo.com>
2014-04-09 18:49:24 -0600

    Loggable comments

--- Loggable.h
+++ Loggable.h
@@ -1,3 +1,14 @@
+//
+//  Loggable.h
+//
+//	See main.cpp file header for credits
+//
+//	Loggable is an interface that can be implemented by any object that need to be logged.
+//	The toString function is pure, which returns a string. The object that implements this function
+//	suppose to return a string of information of itself so the Logger class will call toString function
+//	if the object has been added to loggables vector properly
+//
+
 
 #ifndef HelicopterProject_Loggable_h
 #define HelicopterProject_Loggable_h

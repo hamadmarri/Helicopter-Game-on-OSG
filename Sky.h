df19febe4f999a3190d0d86b8a7ae590e279cea8
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-09 20:45:14 -0600

    Adding more comments in Sky.h and Sky.cpp

--- Sky.h
+++ Sky.h
@@ -5,20 +5,20 @@
 //
 //  A sky calss to represent the sky in our sceen,
 //	it has only one constuctor to set the position and call the model from a file.
-//
+//  A Sky class inhertitance from Model public.
 
 
 #ifndef __HelicopterProject__Sky_h__
 #define __HelicopterProject__Sky_h__
 
 #include "Model.h"
-
+// define Game class
 class Game;
-
+// public inheritance from Model class
 class Sky : public Model {
 public:
-    Sky(Game *game);
-    ~Sky();
+    Sky(Game *game);  // constructor
+    ~Sky();           // deconstructor
 };
 
 

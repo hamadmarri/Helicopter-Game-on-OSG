ccaf21f8d98bec9daf91e5d5c0c1ffd7a45e4c31
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-09 21:05:27 -0600

    adding more comments in RotorSwitchOff.h and RotorSwitchOff.cpp

--- RotorSwitchOff.cpp
+++ RotorSwitchOff.cpp
@@ -1,14 +1,17 @@
 //	See main.cpp file header for credits
+//  This is implementation of RotorSwitchOff class. Implementing two main fuctions , the constructor to set the rotor ,and
+// the boolean function to set the magnitude to zero and return true;
 
 #include "RotorSwitchOff.h"
 
+// the constructor passsing the rotor and set our rotor to the passing arguments.
 
 RotorSwitchOff::RotorSwitchOff(Rotor *rotor) {
 	this->rotor = rotor;
 }
 
 
-
+// set rotor magnitude to zero and return true 
 bool RotorSwitchOff::execute() {
 	this->rotor->setMagnitude(0.0);
 	
--- RotorSwitchOff.h
+++ RotorSwitchOff.h
@@ -1,19 +1,22 @@
 //	See main.cpp file header for credits
 
+//   RotorSwitchOff class that will switch off the magnitude of a rotor to zero 0.0.
+//   It has only two main fuctions , constructor and boolean functioin.
+
 #ifndef __HelicopterProject__RotorSwitchOff__
 #define __HelicopterProject__RotorSwitchOff__
 
 #include "Command.h"
 #include "Rotor.h"
 
-
+// RotorSwitchOff intherite from Command class public
 class RotorSwitchOff : public Command {
 public:
-	RotorSwitchOff(Rotor *rotor);
-	bool execute();
+	RotorSwitchOff(Rotor *rotor);  // constructor passing a rotor
+	bool execute();                 // boolean functioin
 	
 private:
-	Rotor *rotor;
+	Rotor *rotor;              // rotor 
 };
 
 #endif /* defined(__HelicopterProject__RotorSwitchOff__) */

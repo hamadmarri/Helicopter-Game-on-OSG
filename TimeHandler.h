6cd9c390607c6e937ae2f806f81f0f3ddc7eb19d
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-09 20:22:43 -0600

    adding comments in TimeHandler.h

--- TimeHandler.h
+++ TimeHandler.h
@@ -4,11 +4,17 @@
 //	See main.cpp file header for credits
 //
 
+/* TimeHandler class inheritis from GUIEventHandler and Obserable class, to manage events. Events like time, get the current
+   time , previous time, notify all observes classes, check collisions, and calculate the duration, time shifting, and
+   test the logger.
+ 
+ */
+
 
 #ifndef __HelicopterProject__TimeHandler__
 #define __HelicopterProject__TimeHandler__
 
-
+// calling chrono library to measure time spans, like: one minute, two hours, or ten milliseconds.
 #include <chrono>
 #include <osgGA/GUIEventHandler>
 #include "Observable.h"
@@ -16,14 +22,16 @@
 
 // define Game
 class Game;
-
+// inheritance public from GUIEventHandler and Observable
 class TimeHandler : public osgGA::GUIEventHandler, public Observable {
 public:
+    // constructor passing a game class
 	TimeHandler(Game *game);
 	
 	virtual bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa, osg::Object*, osg::NodeVisitor*);
 	
 private:
+    
 	std::chrono::time_point<std::chrono::high_resolution_clock> previousTime;
 	std::chrono::time_point<std::chrono::high_resolution_clock> nowTime;
 	Game *game;

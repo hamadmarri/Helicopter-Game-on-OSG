14ea935533de4810d61231718fab770f958c0089
Hamad Almarri <hamadmarri@yahoo.com>
2014-04-09 23:30:06 -0600

    GameController documented

--- GameController.cpp
+++ GameController.cpp
@@ -8,6 +8,7 @@
 #include "GameController.h"
 
 
+
 GameController::GameController(Game *game, Helicopter *helicopter) {
 	this->game = game;
 	this->helicopter = helicopter;
@@ -23,100 +24,133 @@ bool GameController::handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAd
 	// k for key
 	auto k = tolower(ea.getKey());
 	
+	
+	// check if the event is key down
+	// and based on the key pressed by user,
+	// will add proper command to the invoker to
+	// be executed at the end of the function
 	if (e == osgGA::GUIEventAdapter::KEYDOWN) {
 		
+		// if move forward key pressed and mouse control disabled
 		if (k == this->game->getConfiguration()->getKeySettings().movingForward && !this->game->getConfiguration()->isMouseControl())
 			this->commandsInvoker.addCommand(new JoystickMoveForward(this->helicopter->getJoystick()));
 		
+		// if move right key pressed and mouse control disabled
 		if (k == this->game->getConfiguration()->getKeySettings().movingRight && !this->game->getConfiguration()->isMouseControl())
 			this->commandsInvoker.addCommand(new JoystickMoveRight(this->helicopter->getJoystick()));
 		
+		// if move left key pressed and mouse control disabled
 		if (k == this->game->getConfiguration()->getKeySettings().movingLeft && !this->game->getConfiguration()->isMouseControl())
 			this->commandsInvoker.addCommand(new JoystickMoveLeft(this->helicopter->getJoystick()));
 		
+		// if move backward key pressed and mouse control disabled
 		if (k == this->game->getConfiguration()->getKeySettings().movingBackward && !this->game->getConfiguration()->isMouseControl())
 			this->commandsInvoker.addCommand(new JoystickMoveBackward(this->helicopter->getJoystick()));
 			
+		// if rotate left key pressed
         if (k == this->game->getConfiguration()->getKeySettings().rotateLeft)
 			this->commandsInvoker.addCommand(new RotorIncrease(this->helicopter->getTailRotor()));
 		
+		// if rotate right key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().rotateRight)
 			this->commandsInvoker.addCommand(new RotorDecrease(this->helicopter->getTailRotor()));
 		
-		if (k == osgGA::GUIEventAdapter::KEY_Up)
-			this->commandsInvoker.addCommand(new JoystickMoveForward(this->helicopter->getJoystick()));
-
-        if (k == osgGA::GUIEventAdapter::KEY_Down)
-			this->commandsInvoker.addCommand(new JoystickMoveBackward(this->helicopter->getJoystick()));
-        
+        // if reset joystick key pressed
         if (k == this->game->getConfiguration()->getKeySettings().resetJoystick)
 			this->commandsInvoker.addCommand(new JoystickCenter(this->helicopter->getJoystick()));
         
+		// if make rotor speed zero key pressed
         if (k == this->game->getConfiguration()->getKeySettings().zeroRotorSpeed)
 			this->commandsInvoker.addCommand(new RotorSwitchOff(this->helicopter->getMainRotor()));
         
+		// if decrease rotor speed key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().decreaseRotorSpeed)
 			this->commandsInvoker.addCommand(new RotorDecrease(this->helicopter->getMainRotor()));
         
+		// if increase rotor speed key pressed
         if (k == this->game->getConfiguration()->getKeySettings().increaseRotorSpeed)
 			this->commandsInvoker.addCommand(new RotorIncrease(this->helicopter->getMainRotor()));
         
+		// if make rotor speed neutral key pressed
         if (k == this->game->getConfiguration()->getKeySettings().neutralRotorMode)
 			this->commandsInvoker.addCommand(new RotorNeutral(this->helicopter->getMainRotor()));
 		
+		// if fire key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().fire)
 			this->helicopter->fire();
 		
+		// if increment missile's inclination angle key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().incrementInclinationAngle)
 			this->commandsInvoker.addCommand(new MissileCommand(this->helicopter->getMissile(),
 																MissileCommandType::INCREMENT_INCLINATION_ANGLE));
 		
+		// if decrement missile's inclination angle key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().decrementInclinationAngle)
 			this->commandsInvoker.addCommand(new MissileCommand(this->helicopter->getMissile(),
 																MissileCommandType::DECREMENT_INCLINATION_ANGLE));
 		
+		// if increment missile's initial speed key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().incrementMissileInitialSpeed)
 			this->commandsInvoker.addCommand(new MissileCommand(this->helicopter->getMissile(),
 																MissileCommandType::INCREMENT_INITIAL_SPEED));
 		
+		// if decrement missile's initial speed key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().decrementMissileInitialSpeed)
 			this->commandsInvoker.addCommand(new MissileCommand(this->helicopter->getMissile(),
 																MissileCommandType::DECREMENT_INITIAL_SPEED));
 		
+		// if enable friction key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().frictionEnable)
 			this->game->getConfiguration()->activateFriction();
 		
+		// if disable friction key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().frictionDisable)
 			this->game->getConfiguration()->disactivateFriction();
 		
+		// if update key settings key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().updateKeySettings) {
+			
+			// re-initialize key settings
 			this->game->getConfiguration()->initKeySettings();
+			
+			// re-initialize pop up help screen
 			game->initializePopupHelpScreen();
+			
+			// re-initialize camera
 			game->initializeCamera();
+			
+			// delete old hudsManager and instanciate new one
 			delete game->hudsManager;
 			game->hudsManager = new HudsManager(game, 0);
+			
+			// re-initialize helicopter huds (in case changing missile from initial speed to fuel mode)
 			helicopter->initializeHuds();
 		}
 
+		// if show pop up help screen key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().showPopupHelpScreen)
 			game->popupHelpScreen->show();
 		
+		// if hide pop up help screen key pressed
 		if (k == this->game->getConfiguration()->getKeySettings().hidePopupHelpScreen)
 			game->popupHelpScreen->hide();
 	}
 	
-	if (e == osgGA::GUIEventAdapter::KEYUP) {
-//		this->commandsInvoker.addCommand(new JoystickCenter(this->helicopter->getJoystick()));
-//		this->commandsInvoker.executeAll();
-	}
 	
-	//Detect a mouse movement
+	// Detect a mouse movement
 	if (e == osgGA::GUIEventAdapter::MOVE && this->game->getConfiguration()->isMouseControl()) {
 		this->commandsInvoker.addCommand(new JoystickMouseMovement(this->game, this->helicopter->getJoystick(), ea.getX(), ea.getY()));
-//		this->commandsInvoker.executeAll();
 	}
     
+	
+	// execute all commands on queue
 	this->commandsInvoker.executeAll();
 	
+	
+	// if return true, mouse cannot change camera position
     return false;
 }
+
+
+
+
--- GameController.h
+++ GameController.h
@@ -3,15 +3,16 @@
 //
 //	See main.cpp file header for credits
 //
-//	
+//	GameController takes care of controlling the helicopter and other game options such as
+//	hide/show help screen, enable/disable friction, and missile's inclination angle and initial speed
+//	It gets key, mouse events from GUIEventHandler virtual function handle
+//
+
+#ifndef __HelicopterProject__HelicopterController_h__
+#define __HelicopterProject__HelicopterController_h__
 
 
-#ifndef testOsg_HelicopterController_h
-#define testOsg_HelicopterController_h
-
-#include <vector>
-#include <algorithm> // find
-#include <osgGA/GUIEventHandler>
+#include <osgGA/GUIEventHandler> // handle function
 #include "Helicopter.h"
 #include "Observable.h"
 #include "Invoker.h"
@@ -19,20 +20,36 @@
 #include "Configuration/Configuration.h"
 
 
+// define Game
 class Game;
+
 
 class GameController : public osgGA::GUIEventHandler {
 public:
 	
+	// constructor with setting game and helicopter to control
 	GameController(Game *game, Helicopter *helicopter);
 	
+	
+	// implementation of handle function that comes from GUIEventHandler class
 	virtual bool handle(const osgGA::GUIEventAdapter& ea, osgGA::GUIActionAdapter& aa, osg::Object*, osg::NodeVisitor*);
 	
 private:
+	
+	// helicopter to be controlled
 	Helicopter *helicopter;
-	Invoker commandsInvoker;
+	
+	// game to be controlled
 	Game *game;
+	
+	// adding all commands on commandsInvoker and execute them
+	Invoker commandsInvoker;
 };
 
+
+// since interdependent header includes,
+// including Game.h should be after GameController class
 #include "Game.h"
 #endif
+
+

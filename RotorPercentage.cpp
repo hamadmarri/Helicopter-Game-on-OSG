00c99aed5cba1040bc248feb06f09b8faf092ac9
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-09 21:20:21 -0600

    Adding comments to RotorPercentage.h and RotorPercentage.cpp

--- RotorPercentage.cpp
+++ RotorPercentage.cpp
@@ -1,17 +1,19 @@
 //	See main.cpp file header for credits
+// This is the implementation of RotorPercentage class to set the percentage to a rotor instance and a boolean function
+// to return true if that passed valued set.
 
 #include "RotorPercentage.h"
 
-
+// constructor two parameters , rotor, float
 RotorPercentage::RotorPercentage(Rotor *rotor, float percentage) {
-	this->rotor = rotor;
-	this->percentage = percentage;
+	this->rotor = rotor;                // set the passing rotor to our rotor
+	this->percentage = percentage;          // set the passing float to our percentage
 }
 
 
-
+// boolean function
 bool RotorPercentage::execute() {
-	this->rotor->setMagnitudeInPercentage(this->percentage);
-	
+	this->rotor->setMagnitudeInPercentage(this->percentage);  // set our rotor instance to our passing value and return true
+                                                              // other than that return false
 	return true;
 }
--- RotorPercentage.h
+++ RotorPercentage.h
@@ -1,5 +1,10 @@
 //	See main.cpp file header for credits
 
+// RotorPercentage class. The main purpose of this class is to set any value to the rotor percentage and update
+// the percentage from that value.
+// RotorPercentage class inherites from Command classs public.
+
+
 #ifndef __HelicopterProject__RotorPercentage__
 #define __HelicopterProject__RotorPercentage__
 
@@ -9,10 +14,10 @@
 
 class RotorPercentage : public Command {
 public:
-	RotorPercentage(Rotor *rotor, float percentage);
-	bool execute();
+	RotorPercentage(Rotor *rotor, float percentage);   // constructor passing two parameters float, and rotor
+	bool execute();                                     // boolean function
 	
-private:
+private:                    // two private parameters 
 	Rotor *rotor;
 	float percentage;
 };

7d583a4c33c7b2c702aba92c0ab003edd5eadb89
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-09 21:28:17 -0600

    Adding comments to  RotorNeutral.h and  RotorNeutral.cpp

--- RotorNeutral.cpp
+++ RotorNeutral.cpp
@@ -1,14 +1,16 @@
 //	See main.cpp file header for credits
-
+// This is an implementation of RotorNeutral class , the implementation is two main functions, a constructor and
+//  a boolean function. The constructor will set the passing instance of a rotor and set it to our private member
+// rotor , and the boolena function will return true if the natural set or false if not.
 #include "RotorNeutral.h"
 
-
+// the constructor , passing an instance of a rotor and set it to our private member rotor
 RotorNeutral::RotorNeutral(Rotor *rotor) {
 	this->rotor = rotor;
 }
 
 
-
+// a boolena function return true if rotor natural has been set or not
 bool RotorNeutral::execute() {
 	this->rotor->setNeutral();
 	
--- RotorNeutral.h
+++ RotorNeutral.h
@@ -1,18 +1,20 @@
 //	See main.cpp file header for credits
-
+// RotorNaeutral class inherites from Command class. The main purpose of this class is to set the natural variable of
+// a rotor. It has tow main functions, a constructor and a boolean function to set the natural of a rotor and return
+// true or false.
 #ifndef __HelicopterProject__RotorNeutral__
 #define __HelicopterProject__RotorNeutral__
 
 #include "Command.h"
 #include "Rotor.h"
 
-
+// inhertance from Command class
 class RotorNeutral : public Command {
 public:
-	RotorNeutral(Rotor *rotor);
-	bool execute();
+	RotorNeutral(Rotor *rotor);         // a constructor passing an instance of a rotor
+	bool execute();                     // execute boolean function to make sure if natural variable set or not
 	
-private:
+private:            // one private member rotor 
 	Rotor *rotor;
 };
 

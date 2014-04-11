8099af2a64004a1646f272ab38aa4f96926574fd
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-11 01:13:54 -0600

    Adding comments to JoystickMoveForward.h and JoystickMoveForward.cpp

--- JoystickMoveForward.h
+++ JoystickMoveForward.h
@@ -1,4 +1,6 @@
 //	See main.cpp file header for credits
+//  JoystickMoveForward class inheritance public from Command class. The main purpose of this class is to set the
+//  joystick theta to 15.0 degree and set the phi of the joystick to 0.0.
 
 #ifndef __HelicopterProject__JoystickMoveForward_h__
 #define __HelicopterProject__JoystickMoveForward_h__
@@ -6,13 +8,13 @@
 #include "Command.h"
 #include "Joystick.h"
 
-
+// public inheritance from Command class
 class JoystickMoveForward : public Command {
 public:
-	JoystickMoveForward(Joystick *joystick);
-	bool execute();
+	JoystickMoveForward(Joystick *joystick);            // a constructor to set the joystick
+	bool execute();                                     // boolean function to execute the joystick theta, and phi
 	
-private:
+private:                                                // one private member pointer from a Joystick 
 	Joystick *joystick;
 };
 

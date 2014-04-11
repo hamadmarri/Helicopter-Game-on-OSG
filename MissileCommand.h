27a855319b1cc5b1f20f13acc1230c6a3b2acefa
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-10 23:50:31 -0600

    Adding more comments into MissileCommand.h and MissileCommand.cpp

--- MissileCommand.h
+++ MissileCommand.h
@@ -1,3 +1,11 @@
+//  MissileCommand.h
+//
+//
+// This is MissileCommand class. The class inheriates from the Command class public. The main purpose of this class
+// is to determine the Missile command type and translate it to actions depends on the command pass it to it.This
+// done by setting a pointer of a missile instance and a missilecommand type. Then, a function call execute , will be
+// called to determine the type of missile command and execute it by calling funcitons from the missile instance we have
+// it in the class (pointer).
 
 #ifndef __HelicopterProject__MissileCommand_h__
 #define __HelicopterProject__MissileCommand_h__

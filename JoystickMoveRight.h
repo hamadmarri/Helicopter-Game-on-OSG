f1bbf302a3055c7fb6e7ecf8ba270bd4ead829ed
Fares Alotaibi <alotaibi.fares@yahoo.com>
2014-04-11 00:49:37 -0600

    Adding comments into  JoystickMoveRight.h and JoystickMoveRight.cpp

--- JoystickMoveRight.h
+++ JoystickMoveRight.h
@@ -1,4 +1,6 @@
 //	See main.cpp file header for credits
+//  This is JoystickMoveRight class. The main pourpose of the calss is to the joystick theat to 15 degree, and
+// to set the phi to 270. The class inheritance public from a Command class.
 
 #ifndef __HelicopterProject__JoystickMoveRight_h__
 #define __HelicopterProject__JoystickMoveRight_h__
@@ -6,13 +8,13 @@
 #include "Command.h"
 #include "Joystick.h"
 
-
+// inhertitance public from Command class
 class JoystickMoveRight : public Command {
 public:
-	JoystickMoveRight(Joystick *joystick);
-	bool execute();
+	JoystickMoveRight(Joystick *joystick);      // a constructor passing a pointer from a joystick
+	bool execute();                             // execute function to set the theat,phi
 	
-private:
+private:                // one private member pointe of a joystick 
 	Joystick *joystick;
 };
 
